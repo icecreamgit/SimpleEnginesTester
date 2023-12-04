b257c8806e0a384d94f4078515fea3edee103112
Ice Cream <falcon.with.hat@gmail.com>
2023-12-04 13:28:31 +0300

    Start working

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestEngines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int I = 10;
+            int Toverheat = 100;
+            double Hm = 0.01;
+            double Hv = 0.0001;
+            double C = 0.1;
+            List<int> M = new List<int>() { 20, 75, 100, 105, 75, 0 };
+            List<int> V = new List<int>() { 0, 75, 150, 200, 250, 300 };
+            
+        }
+    }
+}
