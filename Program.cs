d1c2e81e5e3b4809d3f43ee114ed3a9b43f006ba
Ice Cream <falcon.with.hat@gmail.com>
2023-12-05 11:18:04 +0300

    It's done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace TestEngines
 {
@@ -8,13 +9,45 @@ namespace TestEngines
         static void Main(string[] args)
         {
             int I = 10;
-            int Toverheat = 100;
+            double Toverheat = 100;
+            double Tstart = ReadFromConsole();
             double Hm = 0.01;
             double Hv = 0.0001;
             double C = 0.1;
             List<int> M = new List<int>() { 20, 75, 100, 105, 75, 0 };
             List<int> V = new List<int>() { 0, 75, 150, 200, 250, 300 };
+
+            IEngine firstEngine = new FirstEngine(I, Toverheat, Hm, Hv, C, M, V);
+
+            StandForHeat standForHeat = new StandForHeat(firstEngine);
+            StandForPower standForPower = new StandForPower(firstEngine);
+
+            double t = standForHeat.MainTestForHeat(Tstart);
+            double maxPower = standForPower.MainTestForPower(Tstart);
+        }
+
+        static double ReadFromConsole()
+        {
+          
+            double value = 0.0;
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+            Console.WriteLine(">> Enter the value of environmental temperature");
+
+            try
+            {
+                value = Convert.ToDouble(Console.ReadLine(), numberFormatInfo);
+            }
+            catch
+            {
+                value = 25.0;
+                Console.WriteLine(">> You've entered wrong value...");
+            }
             
+            return value;
+
         }
     }
 }
